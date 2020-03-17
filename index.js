69266eb6d21bba7c6d3f242f700221c2351b8b2a
Krishnan Rajiyah <krajiyah@gmail.com>
2020-03-17 15:28:19 -0700

    initial commit

--- index.js
+++ index.js
@@ -18,16 +18,16 @@ const getDefaultProxyTable = () => {
 
 class Proxy extends Model {}
 Proxy.init({
-    path: DataTypes.STRING,
-    dst: DataTypes.STRING
+    guid: DataTypes.STRING,
+    url: DataTypes.STRING
 }, { sequelize, modelName: 'proxy' })
 
 const proxyTable = getDefaultProxyTable()
 
 const proxyRouter = function(req) {
-    const path = req.path
-    console.log("PATH: ", path)
-    if (Object.keys(proxyTable).includes(path)) return proxyTable[path]
+    const guid = req.query.guid
+    console.log("guid: ", guid)
+    if (Object.keys(proxyTable).includes(guid)) return proxyTable[guid]
     return defaultRootUrl
 }
  
@@ -35,18 +35,17 @@ const getProxyTable = async() => {
     const proxies = await Proxy.findAll()
     if (proxies.length == 0) return getDefaultProxyTable()
     return proxies.reduce((table, proxy) => {
-        table[domain + proxy.path] = proxy.dst
+        table[proxy.guid] = proxy.url
         return table
     }, {})
 }
 
-const updateNgrokUrl = async (path, dst) => {
-    console.log(path, dst)
-    let proxy = await Proxy.findOne({where:{path}})
+const updateNgrokUrl = async (guid, url) => {
+    let proxy = await Proxy.findOne({where:{guid}})
     if (proxy == null) {
-        proxy = await Proxy.create({path, dst})
+        proxy = await Proxy.create({url, guid})
     } else {
-        proxy.dst = dst
+        proxy.url = url
         await proxy.save()
     }
 }
@@ -70,10 +69,10 @@ const deleteHandler = async(_, res)  => {
 
 const postHandler = async(req, res) => {
     try {
-        const path = req.body.path
-        const dst = req.body.url
+        const guid = req.body.guid
+        const url = req.body.url
         if (!path || !dst) return res.status(400).send("Invalid params")
-        await updateNgrokUrl(path, dst)
+        await updateNgrokUrl(guid, url)
         await refreshInfo()
         res.send("saved")
     } catch(err) {
