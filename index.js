6e4f5c7e4b2b0bde1bf3b67a10ecf14f342b8e89
Krishnan Rajiyah <krajiyah@gmail.com>
2020-03-17 15:49:37 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,43 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { createProxyMiddleware } = require('http-proxy-middleware')
 const { Sequelize, Model, DataTypes } = require('sequelize')
 const app = express()
 const port = process.env.PORT
 const dbUrl = process.env.DATABASE_URL
 const sequelize = new Sequelize(dbUrl, {dialect: 'postgres'})
-const defaultRootUrl = 'https://google.com'
-
-const getDefaultProxyTable = () => {
-    return {
-        bing: 'https://bing.com',
-        yahoo: 'https://yahoo.com'
-    }
-}
 
 class Proxy extends Model {}
 Proxy.init({
     guid: DataTypes.STRING,
     url: DataTypes.STRING
 }, { sequelize, modelName: 'proxy' })
-
-const proxyTable = getDefaultProxyTable()
-
-const proxyRouter = function(req) {
-    const guid = req.query.guid
-    console.log("guid: ", guid)
-    if (Object.keys(proxyTable).includes(guid)) return proxyTable[guid]
-    return defaultRootUrl
-}
- 
-const getProxyTable = async() => {
-    const proxies = await Proxy.findAll()
-    if (proxies.length == 0) return getDefaultProxyTable()
-    return proxies.reduce((table, proxy) => {
-        table[proxy.guid] = proxy.url
-        return table
-    }, {})
-}
 
 const updateNgrokUrl = async (guid, url) => {
     let proxy = await Proxy.findOne({where:{guid}})
@@ -49,30 +22,12 @@ const updateNgrokUrl = async (guid, url) => {
     }
 }
 
-const refreshInfo = async() => {
-    const table = await getProxyTable()
-    Object.assign(proxyTable, table)
-    console.log("TABLE: ", proxyTable)
-}
-
-const deleteHandler = async(_, res)  => {
-    try {
-        await Proxy.destroy({where: {}, truncate: true})
-        await refreshInfo()
-        res.send("cleared proxy table")
-    } catch(err) {
-        console.error(err)
-        res.status(500).send("could not clear proxy table :(")
-    }
-}
-
 const postHandler = async(req, res) => {
     try {
         const guid = req.body.guid
         const url = req.body.url
         if (!guid || !url) return res.status(400).send("Invalid params")
         await updateNgrokUrl(guid, url)
-        await refreshInfo()
         res.send("saved")
     } catch(err) {
         console.error(err)
@@ -80,20 +35,22 @@ const postHandler = async(req, res) => {
     }
 }
 
+const getHandler = async(req, res) => {
+    const guid = req.query.guid
+    if (!guid) return res.status(400).send("please provide ?guid")
+    const proxy = await Proxy.findOne({where:{guid}})
+    if (!proxy) return res.status(400).send("no such guid exists")
+    return res.redirect(proxy.url)
+}
+
 ;(async() => {
     try {
         await sequelize.sync()
-        await refreshInfo()
     } catch(err) {
         console.error(err)
     }
-    app.post('/ngrok', bodyParser.json(), postHandler)
-    app.delete('/ngrok', deleteHandler)
-    app.use(createProxyMiddleware({
-        target: defaultRootUrl,
-        router: proxyRouter,
-        changeOrigin: true,
-        followRedirects: true
-    }))
-    app.listen(port || 3000)
 })()
+
+app.post('/', bodyParser.json(), postHandler)
+app.get('/', getHandler)
+app.listen(port || 3000)
